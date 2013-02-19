dad10e409793af3bfa2c9a12aff0976aa066af57
GabrielWanzek <gabrielwanzek@ymail.com>
2013-02-19 21:45:05 +0100

    more css, game-chooser is templated

--- includes/Content/home.inc.php
+++ includes/Content/home.inc.php
@@ -1,4 +1,29 @@
-<fieldset>
-    <legend>select game</legend>
-    <p>please select the game you want to play.</p>
-</fieldset>
+<h1 class="centered specialelite">select a game</h1>
+    <p class="centered">please select the game you want to play.</p>
+
+<div class="halbe-box">
+    <ul class="games">
+        <li><a href="#">"The dark history of Priva" by John Doe</a><span class="rate">(10min. | &#216; 2,9&#9733;)</span></li>
+        <li><a href="#">"Princess of Blood" by John Doe</a><span class="rate">(90min. | &#216; 5,0&#9733;)</span></li>
+        <li><a href="#">"Wild Western" by John Doe</a><span class="rate">(45min. | &#216; 1,1&#9733;)</span></li>
+        <li><a href="#">"Remove before flight" by John Doe</a><span class="rate">(5min. | &#216; 2,1&#9733;)</span></li>
+        <li><a href="#">"Spactaculous!" by John Doe</a><span class="rate">(1min. | &#216; 0,0&#9733;)</span></li>
+    </ul>
+</div>
+<div class="halbe-box lastbox">
+    <ul class="games">
+        <li><i>game in development (<a href="#">Story</a>)</i></span></li>
+        <li><i>game in development (<a href="#">Story</a>)</i></span></li>
+    </ul>
+</div>
+<div class="clearfix"></div>
+<p>
+    <b>Information:</b>
+    <ul>
+        <li>To protect the system from spamers and haters, you have to finish the game to rate it.</li>
+        <li>The time is a given value by the story author. The actual time depends on you!</li>
+        <li>There is a keyword for each chapter of a game to join it as 'savegame'. Please don't publish the words, because nobody likes cheating!</li><br>
+        <li>You want to include a game story? Read the instructions <a href="#">here</a></li>
+    </ul>
+    
+</p>
